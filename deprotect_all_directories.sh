64d07866d59da898db8a46f8b9479bff18c6467f
Ger Hobbelt <ger@hobbelt.com>
2017-07-10 18:42:49 +0200

    couple fixes + only deprotect PDFs which have already been processed by qiqqa so that we can link original and deprotected copies using the hashed filename of the original!

--- deprotect_all_directories.sh
+++ deprotect_all_directories.sh
@@ -1,11 +1,11 @@
 #! /bin/bash
 #
-# This assumes you have your Qiqqa database tree located at 
+# This assumes you have your Qiqqa database tree located at
 #    ./base/
 # (relative to this script!)
 #
-# This assumes you have your Qiqqa PDF dump/monitor collection directory tree located at 
-#	 ../Sopkonijn/\!QIQQA-pdf-watch-dir/
+# This assumes you have your Qiqqa PDF dump/monitor collection directory tree located at
+#    ../Sopkonijn/\!QIQQA-pdf-watch-dir/
 # (relative to this script!)
 #
 
@@ -21,40 +21,43 @@ echo "Tools directory: $TOOLDIR"
 # qpdf binaries are located in this directory:
 QPDFDIR=$TOOLDIR/qpdf-6.0.0/bin
 if ! test -f $QPDFDIR/qpdf.exe ; then
-	echo "### ERROR: TOOLDIR or QPDFDIR not set correctly or QPDF binary not installed in the expected spot. Aborting."
-	exit 1
+    echo "### ERROR: TOOLDIR or QPDFDIR not set correctly or QPDF binary not installed in the expected spot. Aborting."
+    exit 1
 fi
 
 QIQQA_MONITOR_DIR="$( realpath $TOOLDIR/../Sopkonijn/\!QIQQA-pdf-watch-dir )"
 QIQQA_DOCUMENTS_DIR="$( realpath $TOOLDIR/base/Guest/documents )"
 
 if ! test -d "$QIQQA_DOCUMENTS_DIR" ; then
-	echo "### ERROR: we're not pointing at the documents store of QIQQA itself. Correct the script. Aborting."
-	exit 1
+    echo "### ERROR: we're not pointing at the documents store of QIQQA itself. Correct the script. Aborting."
+    exit 1
 fi
 if ! test -d "$QIQQA_MONITOR_DIR" ; then
-	echo "### ERROR: The path to the directory tree which is monitored by QIQQA for new PDFs is ill configured. Correct the script. Aborting."
-	exit 1
+    echo "### ERROR: The path to the directory tree which is monitored by QIQQA for new PDFs is ill configured. Correct the script. Aborting."
+    exit 1
 fi
 
 set +a
 
 
 
-
-
-for f in $( find "$QIQQA_MONITOR_DIR" -mindepth 1 -maxdepth 1 -type d ! -name __prot ) ; do 
-	echo "Processing directory: $f" 
-	cd "$f"
-	$TOOLDIR/mv_protected_pdf.sh
-done
+#
+# NOTE: wait with deprotecting / decrypting PDFs until Qiqqa has turned the originals into hash-named ones so we can
+# easily link up originals to deprotected copies!
+#
+#
+# for f in $( find "$QIQQA_MONITOR_DIR" -mindepth 1 -maxdepth 1 -type d ! -name __prot ) ; do
+#     echo "Processing directory: $f"
+#     cd "$f"
+#     $TOOLDIR/mv_protected_pdf.sh
+# done
 
 
 # also check the Qiqqa store itself!
 #cd ~/AppData/Local/Quantisle/Qiqqa/Guest/documents
 cd $QIQQA_DOCUMENTS_DIR
 
-echo "Processing QIQQA Store itself: $(pwd)" 
+echo "Processing QIQQA Store itself: $(pwd)"
 $TOOLDIR/mv_protected_pdf.sh
 
 
--- mv_protected_pdf.sh
+++ mv_protected_pdf.sh
@@ -1,46 +1,46 @@
 #! /bin/bash
 #
 # macro to find protected PDFs in the given directory TREE and move them into the newly created __prot subdirectory
-# 
+#
 # https://stackoverflow.com/questions/35594240/how-to-check-if-pdf-is-password-protected-using-static-tools#
 #
 
 
 if ! test -f $QPDFDIR/qpdf.exe ; then
-	echo "### ERROR: QPDFDIR not set correctly or QPDF binary not installed in the expected spot. Aborting."
-	exit 1
+    echo "### ERROR: QPDFDIR not set correctly or QPDF binary not installed in the expected spot. Aborting."
+    exit 1
 fi
 
 
-pushd .               																	2> /dev/null  > /dev/null 
+pushd .                                                                                                         2> /dev/null  > /dev/null
 
 
 
 
 shopt -s globstar
 
-mkdir -p __prot/__decrypted               																		2> /dev/null  > /dev/null 
-grep -e '\/Encrypt' -l -- **/*.pdf  | grep -v -e '__prot/' | xargs --replace=XXX  mv -n XXX __prot/
-rmdir __prot               																										2> /dev/null  > /dev/null
+mkdir -p __prot/__decrypted                                                                                     2> /dev/null  > /dev/null
+grep -e '\/Encrypt' -l -- **/*.pdf | grep -v -e '__prot/' | xargs --replace=XXX  cp -n -- XXX __prot/
+rmdir __prot                                                                                                    2> /dev/null  > /dev/null
 
 # now go and decrypt those PDF files which have not been decrypted yet:
 if test -d __prot ; then
-	cd __prot
-	echo "Going to decrypt all not-yet-decrypted files..."
-	for f in *.pdf ; do
-		if test -f "$f" && ! test -f "__decrypted/$f" ; then
-			echo "Decrypting $f..."
-		  echo qpdf --decrypt  "$f" "__decrypted/$f"
-		fi
-	done
+    cd __prot
+    echo "Going to decrypt all not-yet-decrypted files..."
+    for f in *.pdf ; do
+        if test -f "$f" && ! test -f "__decrypted/$f" ; then
+            echo "Decrypting $f..."
+            $QPDFDIR/qpdf --decrypt "$f" "__decrypted/$f"
+        fi
+    done
 
-	cd ..
-	# remove directories when they're empty, i.e. when there weren't any crypted PDFs to treat:
+    cd ..
+    # remove directories when they're empty, i.e. when there weren't any crypted PDFs to treat:
 
-	rmdir __prot/__decrypted               																		2> /dev/null  > /dev/null 
-	rmdir __prot                          																		2> /dev/null  > /dev/null 
+    rmdir __prot/__decrypted                                                                                    2> /dev/null  > /dev/null
+    rmdir __prot                                                                                                2> /dev/null  > /dev/null
 fi
 
 
-popd               																	2> /dev/null  > /dev/null 
+popd                                                                                                            2> /dev/null  > /dev/null
 
